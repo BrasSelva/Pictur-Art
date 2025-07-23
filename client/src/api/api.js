0f50654b5a06ed8e9f78e604f8d0819f24637c88
JovickT <jovicktchakala@yahoo.com>
2025-07-23 11:49:49 +0200

    [FIX] api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -27,5 +27,20 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+// Ajout du token aussi pour apiForm
+apiForm.interceptors.request.use((config) => {
+  try {
+    const userString = localStorage.getItem('user');
+    const user = userString ? JSON.parse(userString) : null;
+    const token = user?.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (error) {
+    console.warn('Erreur lecture token dans localStorage', error);
+  }
+  return config;
+});
+
 
 export default api;
